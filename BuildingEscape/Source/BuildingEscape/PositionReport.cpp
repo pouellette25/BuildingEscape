b91399b28d8afc8f1f523c016179f70e98b96317
Paul Ouellette PC <pouellette25@gmail.com>
2017-01-22 21:19:48 -0500

    Accessing Object Names

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -20,8 +20,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName);	
 }
 
 
